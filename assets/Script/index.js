1e496d46a12cda568ebd011dcc1ec14f5305b9d4
xin.yang <xin.yang@chukong-inc.com>
2020-04-10 11:21:04 +0800

    优化进入房间和退出房间的流程处理

--- assets/Script/index.js
+++ assets/Script/index.js
@@ -1,13 +1,17 @@
-const appid = '';
-const key = '';
+const appid = "";
+const key = "";
 if (CC_EDITOR) {
   if (!Editor.CocosService_gmeDemo) {
     Editor.CocosService_gmeDemo = true;
     Editor.log("欢迎使用腾讯云 GME 游戏多媒体引擎服务！");
-    Editor.log("这是一个简单的 GME 示例 Demo，通过本示例您可以快速了解如何使用 GME 来实现游戏语音功能！");
+    Editor.log(
+      "这是一个简单的 GME 示例 Demo，通过本示例您可以快速了解如何使用 GME 来实现游戏语音功能！"
+    );
   }
-  if (appid === '' || key === '') {
-    Editor.log("您需要首先从右侧的服务面板开启 腾讯云GME 服务，然后从 腾讯云GME 服务面板前往腾讯云控制台获取 SDK 初始化参数并填写到示例代码中的相关位置，以使 Demo 能够运行。");
+  if (appid === "" || key === "") {
+    Editor.log(
+      "您需要首先从右侧的服务面板开启 腾讯云GME 服务，然后从 腾讯云GME 服务面板前往腾讯云控制台获取 SDK 初始化参数并填写到示例代码中的相关位置，以使 Demo 能够运行。"
+    );
     Editor.log("初始化参数所在位置：assets/Script/index.js 文件最顶部");
   }
 }
@@ -99,7 +103,7 @@ cc.Class({
   },
 
   // use this for initialization
-  onLoad: function () {
+  onLoad: function() {
     this.initGMEEvents();
     this.btnInit.interactable = true;
     this.btnJoin.interactable = false;
@@ -110,23 +114,23 @@ cc.Class({
     this.updateMute();
   },
 
-  initGMEEvents: function () {
+  initGMEEvents: function() {
     if (tencentGME) {
-      tencentGME.on('event', this.onEvent, this);
+      tencentGME.on("event", this.onEvent, this);
     }
   },
 
-  onDestroy: function () {
+  onDestroy: function() {
     this.uninitGMEEvents();
   },
 
-  uninitGMEEvents: function () {
+  uninitGMEEvents: function() {
     if (tencentGME) {
-      tencentGME.off('event', this.onEvent);
+      tencentGME.off("event", this.onEvent);
     }
   },
 
-  initMultiLang: function () {
+  initMultiLang: function() {
     if (this.lang === cc.sys.LANGUAGE_CHINESE) {
       this.langLabelSetup1.string = "步骤1：初始化 GME";
       this.langLabelSetup1BtnLabel.string = "初始化";
@@ -148,20 +152,22 @@ cc.Class({
     }
   },
 
-  initGME: function () {
-    if (appid === '' || key === '') {
+  initGME: function() {
+    if (appid === "" || key === "") {
       this.printLog("please input AppID or key!");
       return;
     }
-    var openid = '' + parseInt(Math.random() * 100000);
+    var openid = "" + parseInt(Math.random() * 100000);
     tencentGME && tencentGME.init(appid, key, openid);
     this.printLog("init tencentGME, appid:" + appid);
-    this.printCode(`tencentGME && tencentGME.init('${appid}', '${key}', '${openid}');`);
+    this.printCode(
+      `tencentGME && tencentGME.init('${appid}', '${key}', '${openid}');`
+    );
     this.btnInit.interactable = false;
     this.btnJoin.interactable = true;
   },
 
-  joinRoom: function () {
+  joinRoom: function() {
     if (this.joined) {
       tencentGME && tencentGME.exitRoom();
       this.printCode(`tencentGME && tencentGME.exitRoom();`);
@@ -176,14 +182,14 @@ cc.Class({
     }
   },
 
-  updateMute: function () {
+  updateMute: function() {
     this.localSprite.node.active = this.micEnabled;
     this.disableLocalSprite.node.active = !this.micEnabled;
     this.remoteSprite.node.active = this.speakerEnabled;
     this.disableRemoteSprite.node.active = !this.speakerEnabled;
   },
 
-  btnLocalStream: function () {
+  btnLocalStream: function() {
     this.micEnabled = !this.micEnabled;
     this.updateMute();
     tencentGME && tencentGME.enableMic(this.micEnabled);
@@ -191,25 +197,27 @@ cc.Class({
     this.printCode(`tencentGME && tencentGME.enableMic(${this.micEnabled});`);
   },
 
-  btnRemoteStream: function () {
+  btnRemoteStream: function() {
     this.speakerEnabled = !this.speakerEnabled;
     this.updateMute();
-    tencentGME && tencentGME.enableSpeaker(this.speakerEnabled)
+    tencentGME && tencentGME.enableSpeaker(this.speakerEnabled);
     this.printLog("Enable Speaker" + this.speakerEnabled ? "mute" : "unmute");
-    this.printCode(`tencentGME && tencentGME.enableSpeaker(${this.speakerEnabled});`);
+    this.printCode(
+      `tencentGME && tencentGME.enableSpeaker(${this.speakerEnabled});`
+    );
   },
 
-  switchLangCN: function () {
+  switchLangCN: function() {
     this.lang = cc.sys.LANGUAGE_CHINESE;
     this.initMultiLang();
   },
 
-  switchLangEN: function () {
+  switchLangEN: function() {
     this.lang = cc.sys.LANGUAGE_ENGLISH;
     this.initMultiLang();
   },
 
-  exitBtnClick: function () {
+  exitBtnClick: function() {
     if (cc.sys.isBrowser) {
       cc.game.restart();
     } else if (cc.sys.isNative) {
@@ -217,7 +225,7 @@ cc.Class({
     }
   },
 
-  printCode: function (code) {
+  printCode: function(code) {
     this.printLog("   ");
     this.printLog("---------- Sample code start ----------");
     this.printLog(code);
@@ -225,7 +233,7 @@ cc.Class({
     this.printLog("   ");
   },
 
-  printLog: function (info) {
+  printLog: function(info) {
     if (this.logs.length > 50) {
       this.logs = [];
       this.logListView.content.removeAllChildren(true);
@@ -233,47 +241,57 @@ cc.Class({
     this.logs.push(info);
     var item = cc.instantiate(this.itemTemplate);
     this.logListView.content.addChild(item);
-    item.getComponent('Item').updateItem(info);
+    item.getComponent("Item").updateItem(info);
     this.logListView.scrollToBottom(0.1);
   },
 
-  onEvent: function (eventType, result) {
+  onEvent: function(eventType, result) {
     switch (eventType) {
       case tencentGME.event.ITMG_MAIN_EVENT_TYPE_ENTER_ROOM:
-        console.log('ITMG_MAIN_EVENT_TYPE_ENTER_ROOM');
+        console.log("ITMG_MAIN_EVENT_TYPE_ENTER_ROOM");
         console.log(JSON.stringify(result));
-        this.btnLocal.interactable = true;
-        this.btnRemote.interactable = true;
-        this.joined = true;
-        this.speakerEnabled = false;
-        this.micEnabled = false;
-        this.initMultiLang();
-        this.updateMute();
+        if (result.result === 0) {
+          this.btnLocal.interactable = true;
+          this.btnRemote.interactable = true;
+          this.joined = true;
+          this.speakerEnabled = false;
+          this.micEnabled = false;
+          tencentGME && tencentGME.enableSpeaker(this.speakerEnabled);
+          tencentGME && tencentGME.enableMic(this.micEnabled);
+          this.initMultiLang();
+          this.updateMute();
+        } else {
+          this.printCode("Join Room failed: " + result.error_info);
+        }
         break;
       case tencentGME.event.ITMG_MAIN_EVENT_TYPE_EXIT_ROOM:
-        console.log('ITMG_MAIN_EVENT_TYPE_EXIT_ROOM');
+        console.log("ITMG_MAIN_EVENT_TYPE_EXIT_ROOM");
         console.log(JSON.stringify(result));
-        this.btnLocal.interactable = false;
-        this.btnRemote.interactable = false;
-        this.joined = false;
-        this.speakerEnabled = false;
-        this.micEnabled = false;
-        this.initMultiLang();
-        this.updateMute();
+        if (result.result === 0) {
+          this.btnLocal.interactable = false;
+          this.btnRemote.interactable = false;
+          this.joined = false;
+          this.speakerEnabled = false;
+          this.micEnabled = false;
+          tencentGME && tencentGME.enableSpeaker(this.speakerEnabled);
+          tencentGME && tencentGME.enableMic(this.micEnabled);
+          this.initMultiLang();
+          this.updateMute();
+        } else {
+          this.printCode("Leave Room failed: " + result.error_info);
+        }
         break;
       case tencentGME.event.ITMG_MAIN_EVENT_TYPE_ROOM_DISCONNECT:
-        console.log('ITMG_MAIN_EVENT_TYPE_ROOM_DISCONNECT');
+        console.log("ITMG_MAIN_EVENT_TYPE_ROOM_DISCONNECT");
         console.log(JSON.stringify(result));
         break;
       case tencentGME.event.ITMG_MAIN_EVNET_TYPE_USER_UPDATE:
-        console.log('ITMG_MAIN_EVNET_TYPE_USER_UPDATE');
+        console.log("ITMG_MAIN_EVNET_TYPE_USER_UPDATE");
         console.log(JSON.stringify(result));
         break;
     }
   },
 
   // called every frame
-  update: function (dt) {
-
-  },
+  update: function(dt) {}
 });
